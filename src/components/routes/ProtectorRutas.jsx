d5d80389adf77ca229ab8a20bbfc70f345be4cf2
Emilse Arias <emilsearias@gmail.com>
2026-01-22 11:19:54 -0300

    corregimos el componenten ProtectorRutas cuando nos logueamos con el backend

--- src/components/routes/ProtectorRutas.jsx
+++ src/components/routes/ProtectorRutas.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router";
 
 const ProtectorRutas = ({usuarioLogueado}) => {
     //pregunto si no estoy logueado
-    if(!usuarioLogueado){
+    if(!usuarioLogueado.nombre){
         return <Navigate to={'/login'}/>
     }
     // si soy efectivamente el admin
